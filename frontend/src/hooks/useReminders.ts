f87d956156d8382ff403131342c45b592b014706
Asaf <asaf27064@gmail.com>
2026-03-23 18:16:32 +0200

    adding and fixing useReminders

---
+++ frontend/src/hooks/useReminders.ts
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import api from "../api/axios";
+import type { Reminder } from "../types";
+
+export const useReminders = () => {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+  const getAllReminders = async (productId: string) => {
+    const res = await api.get(`/api/reminders/product/${productId}`);
+    setReminders(res.data);
+  };
+
+  const createReminder = async (productId: string, daysBefore: number) => {
+    await api.post(`/api/reminders/product/${productId}`, { daysBefore });
+    await getAllReminders(productId);
+  };
+
+  const deleteReminder = async (reminderId: string, productId: string) => {
+    await api.delete(`/api/reminders/${reminderId}`);
+    await getAllReminders(productId);
+  };
+
+  return { reminders, getAllReminders, createReminder, deleteReminder };
+};
